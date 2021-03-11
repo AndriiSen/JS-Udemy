8e1e2f9d2ea6f421e096388e2444db4bc4ebf63d
Andriy <senanven@gmail.com>
2021-03-11 21:22:08 +0200

    vernulsia k obucheniyu

--- Test1/js/script.js
+++ Test1/js/script.js
@@ -1,21 +1,4 @@
  "use strict";
 
-const numberofFilms = +prompt('Сколько фильмов вы уже посмотрели?', 'значение по умолчанию');
-
-const personalMovieDB = {
-    count: numberofFilms,
-    movies: {},
-    actors: {},
-    ganres: [],
-    private: false
-};
-
-const a = prompt('Один из последних просмотреных фильмов', ''),
-      b = prompt('На сколько оцениваете его', ''),
-      c = prompt('Один из последних просмотреных фильмов', ''),
-      d = prompt('На сколько оцениваете его', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+const name = prompt("как тебя зовут?", ''); 
+console.log (5 === '5');
